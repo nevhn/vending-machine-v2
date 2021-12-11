9190edf316aa852936c10aaaa775763ca9257e8d
exhaustcs <exhaustcs@gmail.com>
2021-12-10 22:26:25 -0500

    added client dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./Components/Navbar/Navbar";
 
-function App() {
+const App = () => {
+  /**TODO: Header -> Vending Machine -> item components */
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/">
+          Home
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h1>Vending Machine</h1>
+      </div>
+      <div>
+        <button>Vendor</button>
+      </div>
+    </div>
+  );
+};

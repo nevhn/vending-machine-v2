26d8457132bf61afa1ca629e7cbb186181703154
exhaustcs <exhaustcs@gmail.com>
2021-12-11 20:07:48 -0500

    had to reinit lot of things- clean slate

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
-  /**TODO: Header -> Vending Machine -> item components */
+function App() {
   return (
-    <Router>
-      <Navbar />
+    <BrowserRouter>
       <Routes>
-        <Route exact path="/">
-          Home
-        </Route>
+        <Route path="/">Hello</Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;

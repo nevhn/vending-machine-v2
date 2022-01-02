11508628fea152ff19690de28f21cdbc570ab61f
exhaustcs <exhaustcs@gmail.com>
2022-01-02 14:33:28 -0500

    made customer page mobile ready

--- client/src/pages/Customer/Customer.style.js
+++ client/src/pages/Customer/Customer.style.js
@@ -1,79 +1,94 @@
-import tw from 'tailwind-styled-components'
-import { ReactComponent as DownArrowsIcon } from '../../assets/down-arrows.svg'
+import tw from "tailwind-styled-components";
+import { ReactComponent as DownArrowsIcon } from "../../assets/down-arrows.svg";
 
 export const DownArrowsSvg = tw(DownArrowsIcon)`
 w-6
 h-6
 text-blue-400
-`
+`;
 
 export const Container = tw.div`
-bg-blue-400 md:bg-yellow-300 lg:grid grid-cols-2 bg-pink-500
-`
+
+ grid 
+ md:bg-yellow-300 
+ lg:grid-cols-2 
+ lg:bg-pink-500
+`;
 export const VendingMachineDiv = tw.div`
 grid
 bg-[#F4F4F4]
-w-[533px]
-h-[652px]
-ml-[211px]
-mt-[31px]
+w-[360px]
+h-[520px]
+m-auto
+mt-3
 rounded-md
 shadow-lg
-`
+md:w-[533px]
+md:h-[652px]
+md:ml-[211px]
+md:mt-[31px]
+lg:bg-black
+`;
 
-// grid-co
-// grid-r
 export const InnerDiv = tw.div`
 grid
-grid-cols-4
+grid-cols-3
 bg-white
-h-[465px]
-mx-[7px]
-mt-[37px]
+h-[470px]
 overflow-y-auto
 gap-y-3
-`
+pr-2
+md:mx-[7px]
+md:grid-cols-4
+md:mt-[37px]
+`;
 
 export const TrayDiv = tw.div`
+hidden
 flex
-bg-[#E8E8E8]
 justify-center
 items-center
+w-[200px]
+m-auto
 h-[115px]
 mt-[11px]
-mx-[100px]
+bg-[#E8E8E8]
+md:mx-[100px]
 drop-shadow-sm
-`
+`;
 // mx-[35px]
 
 export const CustomerInputSection = tw.div`
-md:ml-[180px] lg:mt-[370px]
+grid grid-rows-2 mt-6 md:ml-[180px] lg:mt-[370px]
 
-`
+`;
 
 export const MoneyTextDiv = tw.div`
-`
+ mx-auto row-span-1 md:self-center
+`;
 
 export const MoneyTextSpan = tw.span`
 block
-mb-[10px]
 font-bold
 text-2xl
-`
+md:mb-[10px]
+`;
 
 export const MoneySpan = tw.span`
-
-ml-2
-text-[#64F58D]`
+md:ml-2
+text-[#64F58D]`;
 // export const ChangeSpan = tw.span`ml-2`;
 export const InputDiv = tw.div`
-relative 
-flex
-w-full
-flex-wrap
-items-stretch
-mb-3
-`
+row-span-2
+mx-auto
+my-3
+md:relative 
+md:flex
+md:w-full
+md:flex-wrap
+md:items-stretch
+md:mb-3
+`;
 
 export const MoneyInput = tw.input`
 text-center
@@ -92,4 +107,4 @@ focus:ring
 w-[266px]
 shadow-sm
 ml-[9px]
-`
+`;

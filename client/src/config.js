0aae37bd531ac181cd99504587ad49e2eda99b0b
nevhn <51008990+nevhn@users.noreply.github.com>
2023-07-14 09:19:43 -0400

    Update config.js
    Updated backend url (railway)

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const API_URL =
   process.env.REACT_APP_NODE_ENV !== "production"
     ? "http://localhost:8080/api"
-    : "https://vending-machine-api-jmki.onrender.com/api";
+    : "https://vending-machine-v2-production.up.railway.app/api";
 
 // console.log(API_URL);
 export default API_URL;
